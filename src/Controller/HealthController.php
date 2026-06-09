00a321fac071aacaaf4dcd931e64c8e120255b3b
Matas1555 <matas.asaciovas2001@gmail.com>
2026-06-09 17:55:17 +0300

    added health route

---
+++ src/Controller/HealthController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HealthController extends AbstractController
+{
+    #[Route('/health', name: 'check_health')]
+    public function check()
+    {
+        echo "I am okay!";
+    }
+}
